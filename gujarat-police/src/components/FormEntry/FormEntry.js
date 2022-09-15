62e3f8304abf2ad3aaf6bd4cdad1663b774afe1c
PruthviRaj0009 <praj75629@gmail.com>
2022-09-15 16:13:18 +0530

    first page half commit

--- gujarat-police/src/components/FormEntry/FormEntry.js
+++ gujarat-police/src/components/FormEntry/FormEntry.js
@@ -5,24 +5,27 @@ const FormEntry = () => {
     return (
         <div>
             <div><img src="https://en.wikipedia.org/wiki/File:Gujarat_Police_Logo.png"/></div>
-            <div className="container-1">
-                <label htmlFor='district' className='station-label'>District, Division, Sub-Division</label>
+            <div className="combinecontainer">
+                <div className="container-1">
+                    <label htmlFor='district' className='station-label'>District, Division, Sub-Division</label>
+                </div>
+                <div className='container-2'>
+                    <form className='form'>
+                        <div className='from-inputs'>
+                            <label htmlFor='mobileno' className='from-label'>Mobile Number : </label>
+                            <br />
+                            <input
+                                type='text'
+                                name='mobileno'
+                                className='from-input'
+                                placeholder='Enter your mobile number'
+                            />
+                        </div>
+                        <button className='form-input-btn' type='submit'>Login</button>
+                    </form>
+                </div>
             </div>
-            <div className='container-2'>
-                <form className='form'>
-                    <div className='from-inputs'>
-                    <label htmlFor='mobileno' className='from-label'>Mobile Number : </label>
-                    <br/>
-                    <input 
-                        type='text'
-                        name='mobileno'
-                        className='from-input'
-                        placeholder='Enter your mobile number'
-                    />
-                    </div>
-                    <button className='form-input-btn' type='submit'>Login</button>
-                </form>
-            </div>
+            
         </div>
     )
 }
---
+++ gujarat-police/src/components/details/detsils.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './FormEntry.css'
+
+const FormEntry = () => {
+    return (
+        <div>
+            <div><img src="https://en.wikipedia.org/wiki/File:Gujarat_Police_Logo.png" /></div>
+            <div className='container-2'>
+                <form className='form'>
+                    <div className='from-inputs'>
+                        <label htmlFor='mobileno' className='from-label'>Mobile Number : </label>
+                        <br />
+                        <input
+                            type='text'
+                            name='mobileno'
+                            className='from-input'
+                            placeholder='Enter your mobile number'
+                        />
+                    </div>
+                    <button className='form-input-btn' type='submit'>Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default FormEntry;
